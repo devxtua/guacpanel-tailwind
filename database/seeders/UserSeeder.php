8bf5baedf6fb1f536f2dfde7a27d2f6453c2a231
Taki <takida.it@gmail.com>
2025-08-05 14:44:06 +0300

    Миграции

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,27 +3,32 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Event;
 use App\Models\User;
-use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create superuser
+        // Отключаем observer, если он мешает созданию пользователя
+        Event::forget('eloquent.creating: ' . User::class);
+
+        // Создание суперпользователя
         $superuser = User::create([
             'name' => 'Ota',
+            'user_slug' => 'ota',
             'email' => 'ota@example.com',
             'password' => bcrypt('password'),
         ]);
         $superuser->assignRole('superuser');
 
-        // Create regular user
+        // Создание обычного пользователя
         $user = User::create([
             'name' => 'Regular User',
+            'user_slug' => 'regular-user',
             'email' => 'user@example.com',
             'password' => bcrypt('password'),
         ]);
         $user->assignRole('user');
     }
-} 
+}
